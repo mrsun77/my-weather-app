d4a4cac47215dca7a0f49bab286acd783e49037f
Sol Chamorro A <solmcarmoa@gmail.com>
2024-03-12 12:46:15 -0300

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-let searchFormElement = document.querySelector("#search-form");
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#weather-temperature-value");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#weather-city");
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "ecft6c36beca836b6cf3e248oe33ebd0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
+let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Lisbon");
